bcaecac274fc971f1da7e46cfd4637468a21b1a3
0dividedby0 <jason.halcomb@wsu.edu>
2021-07-28 23:31:09 -0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StoryPageComponent } from './story-page/story-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { WeddingPageComponent } from './wedding-page/wedding-page.component';
+import { RsvpPageComponent } from './rsvp-page/rsvp-page.component';
+import { TravelPageComponent } from './travel-page/travel-page.component';
+import { RegistryPageComponent } from './registry-page/registry-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    StoryPageComponent,
+    NavBarComponent,
+    WeddingPageComponent,
+    RsvpPageComponent,
+    TravelPageComponent,
+    RegistryPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  animations: [
+    trigger('fireworkAnimation', [      
+      transition(':enter', [
+        style({ opacity: 1 }),
+        animate(1000)
+      ]),
+      transition(':leave', [
+        animate(1000, style({ opacity: 0 }))
+      ]),
+      state('*', style({ opacity: 1 })),
+    ])
+  ]
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  title = "Jason and Jessica's Wedding";
+  countdown = "340 days 21 hours 12 minutes"
+  fireworksShowing = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.fireworksShowing = false;
+    }, 4000);
+  }
+
+}
