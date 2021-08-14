6b1eeaccf9279932ecb89f033687b44f6a00a611
0dividedby0 <jason.halcomb@wsu.edu>
2021-08-13 23:54:08 -0700

    Adding server code

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StoryPageComponent } from './story-page/story-page.component';
+import { WeddingPageComponent } from './wedding-page/wedding-page.component';
+import { RsvpPageComponent } from './rsvp-page/rsvp-page.component';
+import { TravelPageComponent } from './travel-page/travel-page.component';
+import { RegistryPageComponent } from './registry-page/registry-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'story', component: StoryPageComponent },
+  { path: 'wedding', component: WeddingPageComponent },
+  { path: 'rsvp', component: RsvpPageComponent },
+  { path: 'travel', component: TravelPageComponent },
+  { path: 'registry', component: RegistryPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/currentUser.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class CurrentUser {
-    access_code: string = '';
-    username: string = '';
-    email: string = '';
-    authenticated: number = 0;
-}
