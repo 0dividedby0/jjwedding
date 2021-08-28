6a3fc0901aa81087f0bcd7452bc09c1faa841f49
0dividedby0 <jason.halcomb@wsu.edu>
2021-08-28 13:19:07 -0700

    Using party instead of user, updated routes

--- client/src/app/currentParty.ts
+++ client/src/app/currentParty.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class CurrentUser {
+export class CurrentParty {
     access_code: string = '';
     username: string = '';
     email: string = '';
--- client/src/app/guest.service.ts
+++ client/src/app/guest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CurrentUser } from './currentUser';
+import { CurrentParty } from './currentParty';
 
 interface User {
   access_code: string;
@@ -16,33 +16,33 @@ export class GuestService {
   authenticateGuest(access_code: string): void {
     this.getGuest(access_code)
     .subscribe((data: User) => {
-      this.currentUser.access_code = data.access_code;
-      this.currentUser.email = data.email;
-      this.currentUser.username = data.username;
-      this.currentUser.authenticated = 1;
+      this.currentParty.access_code = data.access_code;
+      this.currentParty.email = data.email;
+      this.currentParty.username = data.username;
+      this.currentParty.authenticated = 1;
       localStorage.setItem("accessCode", data.access_code);
     });
   }
 
   logoutGuest() {
-    this.currentUser.authenticated = 0;
-    this.currentUser.access_code = "";
-    this.currentUser.email = "";
-    this.currentUser.username = "";
+    this.currentParty.authenticated = 0;
+    this.currentParty.access_code = "";
+    this.currentParty.email = "";
+    this.currentParty.username = "";
     localStorage.removeItem("accessCode");
   }
 
   getGuest(access_code: string) {
-      return this.http.  get<User>(`http://75.172.128.175:7318/user/${access_code}`);
-      // return this.http.get<User>(`http://192.168.0.34:7318/user/${access_code}`);
+      // return this.http.get<User>(`http://75.172.128.175:7318/party/${access_code}`);
+      return this.http.get<User>(`http://192.168.0.34:7318/party/${access_code}`);
   }
 
-  updateUser(updates: any) {
-      this.http.post(`http://75.172.128.175:7318/user/${this.currentUser.access_code}`, updates).subscribe(data => {
-      // this.http.post(`http://192.168.0.34:7318/user/${this.currentUser.access_code}`, updates).subscribe(data => {
-          this.authenticateGuest(this.currentUser.access_code);
+  updateParty(updates: any) {
+      // this.http.post(`http://75.172.128.175:7318/party/${this.currentParty.access_code}`, updates).subscribe(data => {
+      this.http.post(`http://192.168.0.34:7318/party/${this.currentParty.access_code}`, updates).subscribe(data => {
+          this.authenticateGuest(this.currentParty.access_code);
       });
   }
 
-  constructor(private http: HttpClient, private currentUser: CurrentUser) { }
+  constructor(private http: HttpClient, private currentParty: CurrentParty) { }
 }
