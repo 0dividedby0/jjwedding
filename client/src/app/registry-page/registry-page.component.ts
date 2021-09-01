cbe768c57084403651d80f8ee088572c53558c69
0dividedby0 <jason.halcomb@wsu.edu>
2021-09-01 16:39:47 -0700

    Updated google analytics, added registry

--- client/src/app/registry-page/registry-page.component.ts
+++ client/src/app/registry-page/registry-page.component.ts
@@ -10,6 +10,15 @@ export class RegistryPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let jqueryScript = document.createElement('script');
+    jqueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+    document.getElementsByTagName('head')[0].appendChild(jqueryScript);
+
+    let registryScript = document.createElement('script');
+    registryScript.id = 'script_myregistry_giftlist_iframe'
+    registryScript.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=Bci0OJSBmuAf2qR21-S_xg2&v=2';
+    registryScript.type = 'text/javascript';
+    document.getElementById('registryContent')!.appendChild(registryScript);
   }
 
 }
